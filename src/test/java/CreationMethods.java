b91445d2c69ce73a9580c4e2860c667a5f25dd6b
tominister <tommyfang2004@gmail.com>
2022-07-20 18:41:12 -0500

    The great one has come...

--- src/test/java/CreationMethods.java
+++ src/test/java/CreationMethods.java
@@ -6,16 +6,14 @@ import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspace;
 
 /*
 CreationMethods (Create keyspace, table, and data)
-Created by Tommy Fang 7/3/2022
-Big thanks to Athulya and Vikas!
+Created by Tommy, Athulya, and Vikas 7/3/22
  */
 
-public class TableMethods {
-
+public class CreationMethods {
     private CqlSession session;
 
     //constructor
-    public TableMethods(CqlSession session){
+    public CreationMethods(CqlSession session){
         this.session=session;
     }
 
@@ -26,27 +24,24 @@ public class TableMethods {
         session.execute(create);
     }
 
-    public void createTable(String keyspace, String table, ArrayList<String>labels, String partitions, String clustering){
-        String use="USE "+keyspace+";";
-        session.execute(use);
-        StringBuilder query=new StringBuilder("CREATE TABLE "+ table+"(");
+    public void createTable(String keyspace, String table, ArrayList<String>labels, String partitions, String clustering, int TTL){
+        StringBuilder query=new StringBuilder("CREATE TABLE "+ keyspace+"."+table+"(");
         for(int i=0;i<labels.size();i++){
             query.append(labels.get(i) +", ");
         }
-        if(clustering.equals("skip")) query.append("PRIMARY KEY(("+partitions+")));");
-        else query.append("PRIMARY KEY(("+partitions+"),"+ clustering+"));");
+        if(clustering.equals("skip")) query.append("PRIMARY KEY(("+partitions+")))");
+        else query.append("PRIMARY KEY(("+partitions+"),"+ clustering+"))");
+        query.append(" WITH default_time_to_live="+TTL+" IF NOT EXISTS;");
         session.execute(String.valueOf(query));
     }
 
-    public void createData(String keyspace, String tableName, ArrayList<String>labels, ArrayList<String>rows) {
-        String use="USE "+keyspace+";";
-        session.execute(use);
-        StringBuilder query = new StringBuilder("INSERT INTO " + tableName + " (");
+    public void createData(String keyspace, String tableName, ArrayList<String>labels, ArrayList<String>row) {
+        StringBuilder query = new StringBuilder("INSERT INTO "+ keyspace+"."+tableName+" (");
         for(int i=0;i<labels.size()-1;i++){
             query.append(labels.get(i) +", ");
         }
         query.append(labels.get(labels.size()-1) + ")");
-        for (String values : rows) {
+        for (String values : row) {
             StringBuilder q = new StringBuilder(query + " VALUES(" + values + ");");
             session.execute(String.valueOf(q));
         }
